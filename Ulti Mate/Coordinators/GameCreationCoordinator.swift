a3b5ef2c4d1cb7fd69bf3349c294dae1d54552ee
travis ouellette <travis.brouellette@gmail.com>
2018-09-24 17:43:43 -0300

    Added viewControllers to the gameCreationCoordinator
    - Did some minor renaming

--- Ulti Mate/Coordinators/GameCreationCoordinator.swift
+++ Ulti Mate/Coordinators/GameCreationCoordinator.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2018 Codeify. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 // MARK: - Class
 final class GameCreationCoordinator: ChildCoordinatable {
@@ -17,6 +17,18 @@ final class GameCreationCoordinator: ChildCoordinatable {
         return appCoordinator.rootViewController
     }
     
+    private lazy var navigationController: UINavigationController = {
+        let navController: UINavigationController = UINavigationController()
+        navController.navigationBar.tintColor = .white
+        navController.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        navController.navigationBar.shadowImage = UIImage()
+        navController.navigationBar.isTranslucent = true
+        navController.view.backgroundColor = UIColor.clear
+        navController.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        navController.navigationBar.barTintColor = AppAppearance.UltiMateLightBlue
+        return navController
+    }()
+    
     var dismissed: (() -> Void)?
     
     // MARK: Life Cycle
@@ -25,20 +37,46 @@ final class GameCreationCoordinator: ChildCoordinatable {
     }
     
     // MARK: Private
-    private func showGameCreation() {
-        let gameCreationViewModel: GameCreationViewModel = GameCreationViewModel()
+    private func showNaming() {
+        let gameCreationViewModel: NamingViewModel = NamingViewModel()
         gameCreationViewModel.dismiss = {
             self.rootViewController.dismissModalChild(animated: true, type: .normal, completion: nil)
             self.dismissed?()
         }
+        gameCreationViewModel.continueToMap = {
+            self.showLocationPicker()
+        }
         
-        let gameCreationViewController: GameCreationViewController = GameCreationViewController(viewModel: gameCreationViewModel)
-        rootViewController.presentChildModally(gameCreationViewController, animated: true, type: .normal, completion: nil)
+        let gameCreationViewController: NamingViewController = NamingViewController(viewModel: gameCreationViewModel)
+        navigationController.setViewControllers([gameCreationViewController], animated: false)
+        
+        rootViewController.presentChildModally(navigationController, animated: true, type: .normal, completion: nil)
+    }
+    
+    private func showLocationPicker() {
+        let pickLocationViewModel: PickLocationViewModel = PickLocationViewModel()
+        pickLocationViewModel.continueToReview = {
+            self.showReview()
+        }
+        
+        let pickLocationViewController: PickLocationViewController = PickLocationViewController(viewModel: pickLocationViewModel)
+        self.navigationController.pushViewController(pickLocationViewController, animated: true)
+    }
+    
+    private func showReview() {
+        let reviewViewModel: ReviewViewModel = ReviewViewModel()
+        reviewViewModel.finish = {
+            self.rootViewController.dismissModalChild(animated: true, type: .normal, completion: nil)
+            self.dismissed?()
+        }
+        
+        let reviewViewController: ReviewViewController = ReviewViewController(viewModel: reviewViewModel)
+        self.navigationController.pushViewController(reviewViewController, animated: true)
     }
     
     // MARK: Public
     func start() {
-        showGameCreation()
+        showNaming()
     }
     
 }
---
+++ Ulti Mate/View Controllers/GameCreation/PickLocationViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  PickLocationViewModel.swift
+//  Ulti Mate
+//
+//  Created by travis ouellette on 9/24/18.
+//  Copyright © 2018 Codeify. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Class
+final class PickLocationViewModel {
+    // MARK: Properties
+    var continueToReview: (() -> Void)?
+    
+    // MARK: Life Cycle
+    init() {
+        
+    }
+}
